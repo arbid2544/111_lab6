7fa67c501a0171e68ee3da14cdc0651506f4b320
arbid2544 <mrarbid.m@ku.th>
2021-03-15 17:19:27 +0700

    Update 111_lab6-5.cpp

--- 111_lab6-5.cpp
+++ 111_lab6-5.cpp
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-	int W,H,i,j,sum=0,max=0;
+	int W,H,i,j,sum=0,max=0,x,y;
 	scanf("%d %d",&W,&H);
 	int w[W][H];
 	for(i=0;i<H;i++)
@@ -10,12 +10,34 @@ int main()
 	   	scanf("%d",&w[i][j]);
 	   	sum+=w[i][j];
 	   }
-	a=sum%(W*H);
-	b=(W*H)%sum;
-	if(a<b)
-	   max=a;
+	   x=sum;
+	   y=sum;
+	for(i=1;i<W*H;i++)
+	{
+	
+		if(sum%(W*H)==0)
+		{
+			//	printf("Ok1");
+			max=sum;
+		    break;
+		}
+		else if((x-i)%(W*H)==0)
+		{
+			//	printf("Ok2");
+			max=x-i;
+			break;
+		}
+		else if((y+i)%(W*H)==0)
+		{
+			//	printf("Ok3");
+			max=y+i;
+			break;
+		}
+	}
+	if(max-sum>sum-max)
+	   max=max-sum;
 	else
-	   max=b;
+	   max=sum-max;
 	printf("%d",max);
 	    
 	return 0;
